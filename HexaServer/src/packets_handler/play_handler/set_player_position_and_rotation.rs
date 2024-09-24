f5075138747cd2b61a18a4603b473021dee25235
Mansitoh <elmantrolgamer.oficialyt@gmail.com>
2024-09-23 23:54:26 -0400

    Refactor packet handling function and remove commented code

--- HexaServer/src/packets_handler/play_handler/set_player_position_and_rotation.rs
+++ HexaServer/src/packets_handler/play_handler/set_player_position_and_rotation.rs
@@ -13,17 +13,6 @@ pub async fn handle(length: i32, buffer: &mut BytesMut, socket: &mut TcpStream, 
     if buffer.remaining() < 33 as usize {
         return Err("not_enough_data".to_string());
     }
-    /*let mut reader = PacketReader::new(buffer);
-    let x = reader.read_double();
-    let y = reader.read_double();
-    let z = reader.read_double();
-    let yaw = reader.read_float();
-    let pitch = reader.read_float();
-    let mut on_ground = false;
-    if reader.buf.remaining() >= 1 {
-        on_ground = reader.read_boolean();  
-    }
-    println!("x: {}, y: {}, z: {}, yaw: {}, pitch: {}, on_ground: {}", x, y, z, yaw, pitch, on_ground);*/
     let packet = SetPlayerPositionAndRotationPacket::read_packet(buffer,client.get_protocol_version());
     println!("x: {}, y: {}, z: {}, yaw: {}, pitch: {}, on_ground: {}", packet.get_x(), packet.get_y(), packet.get_z(), packet.get_yaw(), packet.get_pitch(), packet.get_on_ground());
     Ok(())
